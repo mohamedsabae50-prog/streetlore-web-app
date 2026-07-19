75b0eaf88a522956397172a90355a0dde4187e97
Mohamed Hamdy <mohamedsabae50@gmail.com>
2026-07-19 17:06:19 +0300

    Upload web directly

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2108841508" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "398568803" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
